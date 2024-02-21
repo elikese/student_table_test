f09f7197d808b509a52e1633d07b036100e0881d
elikese <elikese@gmail.com>
2024-02-21 17:33:00 +0900

    feat:submit버튼

--- src/pages/StudentArrayPage2.jsx
+++ src/pages/StudentArrayPage2.jsx
@@ -1,45 +1,41 @@
 import React, { useEffect, useRef, useState } from 'react';
 
 function StudentArrayPage2() {
-    const [ inputValue, setInputValue ] = useState({
-        id:0,
+    const [inputValue, setInputValue] = useState({
+        id: 0,
         name: "",
         score: 0
     });
 
-    const [ stateFlag , setStateFlag ] = useState(false);
-
-    const [ scoreData, setScoreData ] = useState({
+    const [scoreData, setScoreData] = useState({
         total: 0,
         avg: 0
     });
 
-    const [ studentList, setStudentList ] = useState([]);
+    const [studentList, setStudentList] = useState([]);
 
-    useEffect(()=>{
-        if(stateFlag) {
-            let sum = 0;
-            studentList.forEach(student => sum += parseInt(student.score))
-            let average=(sum/studentList.length).toFixed(2);
-            const newScoreData = {total: sum, avg:average}
-            setScoreData(newScoreData);
-        }
-        setStateFlag(true);
-    },[studentList]);
+    useEffect(() => {
+        studentList.reduce((result, student) => result + student.score, 0)
+        let sum = 0;
+        studentList.forEach(student => sum += parseInt(student.score))
+        let average = sum === 0 ? 0 : (sum / studentList.length);
+        const newScoreData = { total: sum, avg: average }
+        setScoreData(newScoreData);
+    }, [studentList]);
 
-    const [ updateId, setUpdateId ] = useState(0);
+    const [updateId, setUpdateId] = useState(0);
     const staticId = useRef(0);
-    
+
 
     const handleInputChange = (e) => {
-        const {name, value} = e.target;
-        setInputValue({...inputValue, [name]: value})
+        const { name, value } = e.target;
+        setInputValue({ ...inputValue, [name]: value });
     }
 
     const handleAddClick = () => {
-        setStudentList([...studentList, {...inputValue, id: staticId.current}])
+        setStudentList([...studentList, { ...inputValue, id: staticId.current }])
         setUpdateId(staticId.current);
-        staticId.current += 1
+        staticId.current += 1;
     }
 
     const handleDeleteClick = (id) => {
@@ -47,13 +43,13 @@ function StudentArrayPage2() {
     }
 
     const handleEditClick = (id) => {
-        setInputValue(studentList.filter(student => id === student.id)[0]);
         setUpdateId(id);
+        setInputValue(studentList.filter(student => id === student.id)[0]);
     }
 
     const handleCancelClick = () => {
         setInputValue({
-            id:0,
+            id: 0,
             name: "",
             score: 0
         });
@@ -61,18 +57,23 @@ function StudentArrayPage2() {
     }
 
     const handleEditSubmitClick = () => {
-        const findIndex = studentList.indexOf(studentList.filter(student => student.id === updateId)[0])
-        const updateStudentList = [...studentList];
-        updateStudentList[findIndex] = inputValue;
-        setStudentList(updateStudentList);
+        // 방법1
+        // setStudentList([...studentList.filter(student => student.id !== updateId), { ...inputValue, id: updateId }].sort((a, b) => a.id - b.id))
+        // 방법2
+        setStudentList(studentList.map(student => student.id !== updateId ? student : { ...inputValue, id: updateId }));
+        // 방법3
+        // const findIndex = studentList.indexOf(studentList.filter(student => student.id === updateId)[0])
+        // const updateStudentList = [...studentList];
+        // updateStudentList[findIndex] = inputValue;
+        // setStudentList(updateStudentList);
     }
 
     return (
         <div>
             <div>
-                <input type="text" value={inputValue.id} name='id' placeholder='id' disabled/>
-                <input type="text" value={inputValue.name} name='name' placeholder='이름'onChange={handleInputChange}/>
-                <input type="text" value={inputValue.score} name='score' placeholder='점수'onChange={handleInputChange}/>
+                <input type="text" value={inputValue.id} name='id' placeholder='id' disabled />
+                <input type="text" value={inputValue.name} name='name' placeholder='이름' onChange={handleInputChange} />
+                <input type="text" value={inputValue.score} name='score' placeholder='점수' onChange={handleInputChange} />
                 <button onClick={handleAddClick}>추가</button>
             </div>
             <table>
@@ -85,21 +86,21 @@ function StudentArrayPage2() {
                 </thead>
                 <tbody>
                     {studentList.map(student => {
-                         return(
+                        return (
                             <tr key={student.id}>
                                 <td>{student.id}</td>
                                 <td>{student.name}</td>
                                 <td>{student.score}</td>
                                 <td>
-                                    { updateId !== student.id 
-                                        ?<button onClick={() => handleEditClick(student.id)}>수정</button>
-                                        :<button onClick={handleEditSubmitClick}>확인</button>
+                                    {updateId !== student.id
+                                        ? <button onClick={() => handleEditClick(student.id)}>수정</button>
+                                        : <button onClick={handleEditSubmitClick}>확인</button>
                                     }
                                 </td>
                                 <td>
-                                    { updateId !== student.id
-                                        ?<button onClick={() => {handleDeleteClick(student.id)}}>삭제</button>
-                                        :<button onClick={handleCancelClick}>취소</button>
+                                    {updateId !== student.id
+                                        ? <button onClick={() => { handleDeleteClick(student.id) }}>삭제</button>
+                                        : <button onClick={handleCancelClick}>취소</button>
                                     }
                                 </td>
                             </tr>
@@ -113,7 +114,7 @@ function StudentArrayPage2() {
                     </tr>
                     <tr>
                         <th>평균</th>
-                        <th colSpan={2}>{scoreData.avg}</th>
+                        <th colSpan={2}>{scoreData.avg.toFixed(2)}</th>
                     </tr>
                 </tfoot>
             </table>
